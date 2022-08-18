af641f297b16bd79b807a0aa441b6b9a98dccedd
yslee <ptss37@gmail.com>
2022-08-18 17:54:35 +0900

    modify px4flow update rate from 20 to 10

--- src/gazebo_ros_px4flow.cpp
+++ src/gazebo_ros_px4flow.cpp
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
-*/
+ */
 #ifdef _WIN32
-  // Ensure that Winsock2.h is included before Windows.h, which can get
-  // pulled in by anybody (e.g., Boost).
+// Ensure that Winsock2.h is included before Windows.h, which can get
+// pulled in by anybody (e.g., Boost).
 #include <Winsock2.h>
 #endif
 
@@ -37,9 +37,8 @@ GZ_REGISTER_SENSOR_PLUGIN(Px4FlowPlugin)
 
 /////////////////////////////////////////////////
 Px4FlowPlugin::Px4FlowPlugin()
-: SensorPlugin(), width(0), height(0), depth(0)
+    : SensorPlugin(), width(0), height(0), depth(0)
 {
-
 }
 
 /////////////////////////////////////////////////
@@ -92,9 +91,10 @@ void Px4FlowPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf)
   const string scopedName = _sensor->GetParentName();
 #endif
 
-  focal_length_ = (this->width/2)/tan(hfov_/2);
+  focal_length_ = (this->width / 2) / tan(hfov_ / 2);
 
-  if (this->width != 64 || this->height != 64) {
+  if (this->width != 64 || this->height != 64)
+  {
     gzerr << "[gazebo_optical_flow_plugin] Incorrect image size, must by 64 x 64.\n";
   }
 
@@ -103,14 +103,17 @@ void Px4FlowPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf)
   else
     gzwarn << "[gazebo_optical_flow_plugin] Please specify a robotNamespace.\n";
 
-  if (_sdf->HasElement("outputRate")) {
+  if (_sdf->HasElement("outputRate"))
+  {
     output_rate_ = _sdf->GetElement("outputRate")->Get<int>();
-  } else {
+  }
+  else
+  {
     output_rate_ = DEFAULT_RATE;
     gzwarn << "[gazebo_optical_flow_plugin] Using default output rate " << output_rate_ << ".";
   }
 
-  if(_sdf->HasElement("hasGyro"))
+  if (_sdf->HasElement("hasGyro"))
     has_gyro_ = _sdf->GetElement("hasGyro")->Get<bool>();
   else
     has_gyro_ = HAS_GYRO;
@@ -118,8 +121,9 @@ void Px4FlowPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf)
   node_handle_ = transport::NodePtr(new transport::Node());
   node_handle_->Init(namespace_);
 
-  if(has_gyro_) {
-    if(_sdf->HasElement("hasGyro"))
+  if (has_gyro_)
+  {
+    if (_sdf->HasElement("hasGyro"))
       gyro_sub_topic_ = _sdf->GetElement("gyroTopic")->Get<std::string>();
     else
       gyro_sub_topic_ = kDefaultGyroTopic;
@@ -156,43 +160,44 @@ void Px4FlowPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf)
 
   this->pub_ = this->rosnode_->advertise<kari_estimator::optical_flow>("/px4flow", 1);
 
-  //init flow
-  // optical_flow_ = new OpticalFlowOpenCV(focal_length_, focal_length_, output_rate_);
+  // init flow
+  //  optical_flow_ = new OpticalFlowOpenCV(focal_length_, focal_length_, output_rate_);
   optical_flow_ = new OpticalFlowPX4(focal_length_, focal_length_, output_rate_, this->width);
 }
 
 /////////////////////////////////////////////////
-void Px4FlowPlugin::OnNewFrame(const unsigned char * _image,
-                              unsigned int _width,
-                              unsigned int _height,
-                              unsigned int _depth,
-                              const std::string &_format)
+void Px4FlowPlugin::OnNewFrame(const unsigned char *_image,
+                               unsigned int _width,
+                               unsigned int _height,
+                               unsigned int _depth,
+                               const std::string &_format)
 {
 
-  //get data depending on gazebo version
-  #if GAZEBO_MAJOR_VERSION >= 7
-    _image = this->camera->ImageData(0);
-    double frame_time = this->camera->LastRenderWallTime().Double();
-  #else
-    _image = this->camera->GetImageData(0);
-    double frame_time = this->camera->GetLastRenderWallTime().Double();
-  #endif
+// get data depending on gazebo version
+#if GAZEBO_MAJOR_VERSION >= 7
+  _image = this->camera->ImageData(0);
+  double frame_time = this->camera->LastRenderWallTime().Double();
+#else
+  _image = this->camera->GetImageData(0);
+  double frame_time = this->camera->GetLastRenderWallTime().Double();
+#endif
 
-  frame_time_us_ = (frame_time - first_frame_time_) * 1e6; //since start
+  frame_time_us_ = (frame_time - first_frame_time_) * 1e6; // since start
 
   float flow_x_ang = 0.0f;
   float flow_y_ang = 0.0f;
-  //calculate angular flow
-  int quality = optical_flow_->calcFlow((uchar*)_image, frame_time_us_, dt_us_, flow_x_ang, flow_y_ang);
+  // calculate angular flow
+  int quality = optical_flow_->calcFlow((uchar *)_image, frame_time_us_, dt_us_, flow_x_ang, flow_y_ang);
 
-  if (quality >= 0) { // calcFlow(...) returns -1 if data should not be published yet -> output_rate
-    //prepare optical flow message
-    // Get the current simulation time.
-    #if GAZEBO_MAJOR_VERSION >= 9
-      common::Time now = world->SimTime();
-    #else
-      common::Time now = world->GetSimTime();
-    #endif
+  if (quality >= 0)
+  { // calcFlow(...) returns -1 if data should not be published yet -> output_rate
+// prepare optical flow message
+//  Get the current simulation time.
+#if GAZEBO_MAJOR_VERSION >= 9
+    common::Time now = world->SimTime();
+#else
+    common::Time now = world->GetSimTime();
+#endif
 
     opticalFlow_message.set_time_usec(now.Double() * 1e6);
     opticalFlow_message.set_sensor_id(2.0);
@@ -206,20 +211,22 @@ void Px4FlowPlugin::OnNewFrame(const unsigned char * _image,
     px4flow_msg_.integrated_x = quality ? flow_x_ang : 0.0f;
     px4flow_msg_.integrated_y = quality ? flow_y_ang : 0.0f;
 
-
-    if(has_gyro_) {
+    if (has_gyro_)
+    {
       opticalFlow_message.set_integrated_xgyro(opticalFlow_rate.X());
       opticalFlow_message.set_integrated_ygyro(opticalFlow_rate.Y());
       opticalFlow_message.set_integrated_zgyro(opticalFlow_rate.Z());
 
-     px4flow_msg_.integrated_xgyro = opticalFlow_rate.X();
-     px4flow_msg_.integrated_ygyro = opticalFlow_rate.Y();
-     px4flow_msg_.integrated_zgyro = opticalFlow_rate.Z();
+      px4flow_msg_.integrated_xgyro = opticalFlow_rate.X();
+      px4flow_msg_.integrated_ygyro = opticalFlow_rate.Y();
+      px4flow_msg_.integrated_zgyro = opticalFlow_rate.Z();
 
-      //reset gyro integral
+      // reset gyro integral
       opticalFlow_rate.Set();
-    } else {
-      //no gyro
+    }
+    else
+    {
+      // no gyro
       opticalFlow_message.set_integrated_xgyro(NAN);
       opticalFlow_message.set_integrated_ygyro(NAN);
       opticalFlow_message.set_integrated_zgyro(NAN);
@@ -227,13 +234,12 @@ void Px4FlowPlugin::OnNewFrame(const unsigned char * _image,
       px4flow_msg_.integrated_xgyro = NAN;
       px4flow_msg_.integrated_ygyro = NAN;
       px4flow_msg_.integrated_zgyro = NAN;
-
     }
     opticalFlow_message.set_temperature(20.0f);
     opticalFlow_message.set_quality(quality);
     opticalFlow_message.set_time_delta_distance_us(0);
-    opticalFlow_message.set_distance(0.0f); //get real values in gazebo_mavlink_interface.cpp
-    //send message
+    opticalFlow_message.set_distance(0.0f); // get real values in gazebo_mavlink_interface.cpp
+    // send message
     opticalFlow_pub_->Publish(opticalFlow_message);
 
     px4flow_msg_.temperature = 20.0f;
@@ -242,21 +248,17 @@ void Px4FlowPlugin::OnNewFrame(const unsigned char * _image,
     px4flow_msg_.distance = 0.0f;
 
     this->pub_.publish(this->px4flow_msg_);
-
-    ROS_INFO_NAMED("px4flow", "px4flow");
-
   }
-
-
 }
 
-void Px4FlowPlugin::ImuCallback(ConstIMUPtr& _imu) {
-  //accumulate gyro measurements that are needed for the optical flow message
-  #if GAZEBO_MAJOR_VERSION >= 9
-    common::Time now = world->SimTime();
-  #else
-    common::Time now = world->GetSimTime();
-  #endif
+void Px4FlowPlugin::ImuCallback(ConstIMUPtr &_imu)
+{
+// accumulate gyro measurements that are needed for the optical flow message
+#if GAZEBO_MAJOR_VERSION >= 9
+  common::Time now = world->SimTime();
+#else
+  common::Time now = world->GetSimTime();
+#endif
 
   uint32_t now_us = now.Double() * 1e6;
   ignition::math::Vector3d px4flow_gyro = ignition::math::Vector3d(_imu->angular_velocity().x(),
@@ -266,7 +268,8 @@ void Px4FlowPlugin::ImuCallback(ConstIMUPtr& _imu) {
   static uint32_t last_dt_us = now_us;
   uint32_t dt_us = now_us - last_dt_us;
 
-  if (dt_us > 1000) {
+  if (dt_us > 1000)
+  {
     opticalFlow_rate += px4flow_gyro * (dt_us / 1000000.0f);
     last_dt_us = now_us;
   }
